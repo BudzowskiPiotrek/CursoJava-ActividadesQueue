c7ff12c0f29be4ae9f56e878775622390a756083
Piotrek Budzowski <piobudzows@gmail.com>
2025-03-26 20:51:40 +0100

    Create Tarea.java

---
+++ Actividad 2/Tarea.java
@@ -0,0 +1,24 @@
+class Tarea implements Comparable<Tarea> {
+    private String descripcion;
+    private int prioridad;
+
+    public Tarea(String descripcion, int prioridad) {
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    @Override
+    public int compareTo(Tarea otra) {
+        return Integer.compare(this.prioridad, otra.prioridad);
+    }
+
+
+}
